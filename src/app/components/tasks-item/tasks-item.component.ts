da625ecdfb2fd1cfde339520ef1c81d6a7dc2b1a
Subash <Subashtech14@gmail.com>
2023-01-20 16:06:26 +0530

    Message has been added

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Task } from 'src/app/Task';
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent {
+  @Input()
+  task!: Task;
+constructor(){
+  
+}
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASKS: Task[] =[
+  {
+    id:1,
+    text:'Doctor Appointment',
+    day : 'May 5th at 2:30 P.M',
+    reminder :true
+  },
+  {
+    id:2,
+    text:'Meeting at School',
+    day : 'May 6th at 1:30 P.M',
+    reminder :true
+  },
+  {
+    id:3,
+    text:'Doctor Appointment',
+    day : 'May 5th at 2:30 P.M',
+    reminder :true
+  },
+   {
+    id:4,
+    text:'Doctor Appointment',
+    day : 'May 5th at 2:30 P.M',
+    reminder :true
+  }
+]
